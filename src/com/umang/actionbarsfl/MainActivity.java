06b222357fdd119529b0b7b4313f2076cb29a641
umangjeet singh pahwa <umangjeet.pahwa@locationlabs.com>
2014-06-04 13:15:53 +0530

    using search widget in search activity itself

--- src/com/umang/actionbarsfl/MainActivity.java
+++ src/com/umang/actionbarsfl/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import android.support.v4.view.MenuItemCompat;
+import android.app.SearchManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBarActivity;
@@ -25,6 +26,13 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+      Intent intetn = getIntent();
+      if(Intent.ACTION_SEARCH.equals(intetn.getAction())) {
+
+         String  query = intetn.getStringExtra(SearchManager.QUERY);
+         Toast.makeText(MainActivity.this, query, Toast.LENGTH_LONG).show();
+
+      }
 		setContentView(R.layout.activity_main);
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayShowHomeEnabled(true);
